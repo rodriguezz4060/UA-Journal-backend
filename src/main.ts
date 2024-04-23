3c3659fc8775a90e6a6a98d6ec7ca4f5b2836590
Igor Strelnyk <59284261+rodriguezz4060@users.noreply.github.com>
2024-04-23 11:14:04 +0300

    test deploy

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule)
 
-  app.enableCors({
-    origin: [/^(.*)/],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 200,
-    credentials: true,
-    allowedHeaders:
-      'Origin,X-Requested-With,Content-Type,Accept,Authorization,authorization,X-Forwarded-for',
-  });
+	app.enableCors({
+		origin: 'https://ua-journal-frontend.vercel.app/',
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		preflightContinue: false,
+		optionsSuccessStatus: 200,
+		credentials: true,
+		allowedHeaders:
+			'Origin,X-Requested-With,Content-Type,Accept,Authorization,authorization,X-Forwarded-for'
+	})
 
-  app.useGlobalPipes(new ValidationPipe());
+	app.useGlobalPipes(new ValidationPipe())
 
-  await app.listen(7777);
+	await app.listen(7777)
 }
 
-bootstrap();
+bootstrap()
