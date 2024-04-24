3100f5a75e285af59674829b01ee5967fd915b6b
Igor Strelnyk <59284261+rodriguezz4060@users.noreply.github.com>
2024-04-24 09:54:00 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
 	app.enableCors({
-		origin: 'https://ua-journal-frontend.vercel.app/',
+		origin: [/^(.*)/],
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 		preflightContinue: false,
 		optionsSuccessStatus: 200,
